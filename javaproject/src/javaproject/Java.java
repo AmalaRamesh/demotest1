93537c2a44f6f8ffcd9e0f221fa70905a946db47
Amala <amalaramesh08@gmail.com>
2023-06-14 19:03:14 +0530

    java

---
+++ javaproject/src/javaproject/Java.java
@@ -0,0 +1,17 @@
+package javaproject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello ");
+
+		WebDriver wb =new ChromeDriver();
+		wb.get("https://www.google.com/");
+		
+
+	}
+
+}
